ac37aa3012606ee4862249fd99d8dc68103aade2
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-07-29 15:11:07 +0300

    Add files via upload

---
+++ DiziEnBuyuk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int[] sayılar = new int[] { 2, 3, 6, 3, 4, 1, 9, 8, 2, 6 };
+            
+
+            Console.WriteLine("en büyük sayı:"+ sayılar.Max());
+            Console.ReadLine();
+        }
+    }
+}
