5e989fc8b1a53c8c18a373509c1eac7494548e77
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-01 07:30:49 +0300

    Add files via upload

---
+++ Dama tahtası kodlaması.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if ((i + j) % 2 == 0) Console.Write("0 ");
+                    else Console.Write("1 ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
