eeb66f7442e2764f2b2bae12e3016957e6c6afaa
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-02 08:32:01 +0300

    Add files via upload

---
+++ Üs Bulma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void ÜsAl(int sayi1, int sayi2) { Console.WriteLine("üssü: " + Math.Pow(sayi1, sayi2)); } 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iki sayı giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            ÜsAl(a, b);
+            Console.ReadLine();
+        }
+    }
+}
