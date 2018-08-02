15db39d6578e1d4da0f2401cc79788a49ac2230a
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-02 09:32:17 +0300

    Add files via upload

---
+++ Dizideki sayıları verilen bir sayı kadar sağa kaydıran program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayılar = new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            Console.WriteLine("dizi ne kadar sağa kaydırılsın");
+            int a = int.Parse(Console.ReadLine());
+            int[] yeni = new int[sayılar.Length + a];
+
+            for (int i = 0; i < a; i++)
+            {
+                yeni[i] = 0;
+            }
+            for (int i = a; i < yeni.Length; i++)
+            {
+                yeni[i] = sayılar[i-a];
+
+            }
+            foreach (int i in yeni)
+            {
+                Console.Write(i+" ");
+            }           
+
+            Console.ReadLine();
+
+        }
+    }
+}
