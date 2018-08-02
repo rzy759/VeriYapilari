cbb02034a021dfb4fbc1b191e6a10b9393c8e024
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-02 08:13:56 +0300

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı giriniz: ");
+            string sayı = Console.ReadLine();
+            
+            char[]sayı1=sayı.ToCharArray();
+            Array.Reverse(sayı1);
+
+            string tekrar = new string(sayı1);
+
+            int a = int.Parse(sayı);
+            int b = int.Parse(tekrar);
+
+            if (sayı == tekrar) Console.WriteLine("bu sayı bir palindrome sayıdır ");
+            else Console.WriteLine("bu sayı bir palindrome sayı değildir ");
+
+            Console.ReadLine();
+        }
+    }
+}
