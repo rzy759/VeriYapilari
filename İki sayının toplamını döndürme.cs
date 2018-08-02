382b0867417fd8c5ce9ab91628a57262003a5551
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-02 08:22:21 +0300

    Add files via upload

---
+++ İki sayının toplamını döndürme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void toplam(int sayı1, int sayı2) { Console.WriteLine("toplam: " +(sayı1 + sayı2)); }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İki sayı giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            toplam(a, b);
+            Console.ReadLine();
+        }
+    }
+}
