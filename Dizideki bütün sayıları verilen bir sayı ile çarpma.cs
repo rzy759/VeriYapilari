95e51549d1702042cd7b1b7b1dbd5b0c1ffa5551
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-03 07:10:29 +0300

    Add files via upload

---
+++ Dizideki bütün sayıları verilen bir sayı ile çarpma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dizi hangi sayı ile çarpılacak: ");
+            int a = int.Parse(Console.ReadLine());
+            int[] sayılar = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            foreach (int i in sayılar)
+            {
+                Console.WriteLine(i*a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
