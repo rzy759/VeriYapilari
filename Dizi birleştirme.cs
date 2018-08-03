7e4b28e976828517a7e0e1d397417cae15b2f7a3
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-03 07:19:32 +0300

    Add files via upload

---
+++ Dizi birleştirme.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi1 = new int[] { 1, 2, 3 };
+            int[] dizi2 = new int[] { 4, 5, 6 };
+            int[] diziler = new int[dizi1.Length + dizi2.Length];
+            for (int i = 0; i < dizi1.Length; i++)
+            {
+                diziler[i] = dizi1[i];
+            }
+            for (int i = dizi1.Length; i < dizi2.Length+dizi1.Length; i++)
+            {
+                diziler[i] = dizi2[i - dizi1.Length];
+            }
+            foreach (int i in diziler)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
