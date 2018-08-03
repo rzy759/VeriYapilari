f683c31473c7ff5959d81811818658bb96665692
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-03 07:59:29 +0300

    Add files via upload

---
+++ Polinom işleyen kod yazma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("polinomun katsayılarını boşluk koyarak giriniz: ");
+            string katsayı = Console.ReadLine();
+            string[] katsayılar = katsayı.Split(' ');
+            double pol = 0;
+            for (int i = 0; i < katsayılar.Length; i++)
+            {
+                Console.Write(katsayılar[i]+"x^"+(katsayılar.Length-i)+" ");
+            }
+            
+            Console.WriteLine(", x in değerini giriniz: ");
+            double x = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < katsayılar.Length; i++)
+            {
+                pol += int.Parse(katsayılar[i]) * Math.Pow(x, katsayılar.Length - i);
+            }
+
+          
+            Console.WriteLine("polinomun değeri: "+pol);
+            Console.ReadLine();
+        }
+    }
+}
