2c896f49746e5bc5589904fece9b7f87360c8ee7
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-04 21:37:05 +0300

    Add files via upload

---
+++ Karmaşık sayıları toplama.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("iki tane karmaşık sayı giriniz: ");
+            Console.Write("gerçek kısmı: "); int a = int.Parse(Console.ReadLine());
+            Console.Write("sanal kısmı: "); int b = int.Parse(Console.ReadLine());
+            Console.Write("gerçek kısmı: "); int c = int.Parse(Console.ReadLine());
+            Console.Write("sanal kısmı: "); int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a+"+"+b+"i");
+            Console.WriteLine(c+"+"+d+"i");
+
+
+            Console.WriteLine("toplamı= "+(a+c)+"+"+(b+d)+"i");
+            Console.ReadLine();
+        }
+
+    }
+}
