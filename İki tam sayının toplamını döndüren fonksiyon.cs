a63820a54b93962b479d5ef40b97f84284f0ada7
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-05 21:57:23 +0300

    Add files via upload

---
+++ İki tam sayının toplamını döndüren fonksiyon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {    
+        static int toplam(int a,int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("2 sayı giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+                       
+            Console.WriteLine(toplam(a, b));
+            Console.ReadLine();
+        }
+    }
+}
