206b8eca713e745eedf166fbb2ad99a5a5824817
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-05 21:48:29 +0300

    Add files via upload

---
+++ Dizideki elemanları ekrana basma.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void ekranaBas(int[] dizi)
+        {
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+        static void ekranaBas(string[] dizi)
+        {
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] metin = new string[] { "ali", "veli", "ahmet" };
+            int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ekranaBas(metin);
+            Console.WriteLine("----------");
+            ekranaBas(data);
+
+            Console.ReadLine();
+        }
+    }
+}
