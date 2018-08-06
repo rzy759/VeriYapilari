a8e8fe6f5b380743b5fbf85e7d12934d098c30cc
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-06 08:33:51 +0300

    Add files via upload

---
+++ Bir dizgi ve bir sayı alarak, verilen sayıdaki harfi döndüren fonksiyon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void harfDön(string s,int i)
+        {
+            Console.WriteLine(s[i-1]);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir dizgi giriniz: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("bir sayı giriniz: ");
+            int i = int.Parse(Console.ReadLine());
+            
+            harfDön(s, i);
+            Console.ReadLine();
+        }
+    }
+}
