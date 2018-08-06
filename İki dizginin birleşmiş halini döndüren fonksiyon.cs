2ff4c7c674ab93fa0ec5df12573e4c5b07fce42e
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-06 08:51:24 +0300

    Add files via upload

---
+++ İki dizginin birleşmiş halini döndüren fonksiyon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void birleştir(string s1,string s2)
+        {
+            Console.WriteLine(s1+" "+s2);
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iki dizgi giriniz: ");
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+
+            birleştir(s1, s2);
+            Console.ReadLine();
+        }
+    }
+}
