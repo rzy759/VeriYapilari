adbff13bbaea74877f1265ddb78ff953b0b1cd2e
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-08 07:31:25 +0300

    Add files via upload

---
+++ Klavyeden e-posta adresi okuyarak, posta adresindeki kullanıcı ve hostu ekrana yazma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("email adresi: ");
+            string s1 = Console.ReadLine();
+            string[] S = s1.Split('@');
+            Console.WriteLine("kullanıcı adı: "+S[0]);
+            Console.WriteLine("hostu: "+S[1]);
+            Console.ReadLine();
+        }
+    }
+}
