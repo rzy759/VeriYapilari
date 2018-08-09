df77186208c80a6693f9d528b0c36b3680ad01f4
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-09 07:56:30 +0300

    Add files via upload

---
+++ Çalışan bilgilerinin tutulduğu ADT tasarlama.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("T.C kimlik no: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("İsim: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Soyisim: ");
+            string c = Console.ReadLine();
+            Console.WriteLine("Maaş: ");
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine("Çalıştığı departman: ");
+            string e = Console.ReadLine();
+
+            ArrayList liste = new ArrayList();
+            liste.Add(a);
+            liste.Add(b);
+            liste.Add(c);
+            liste.Add(d);
+            liste.Add(e);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("aradığınız bilgi: ");
+            var f = Console.ReadLine();         
+            if (liste.Contains(f)) Console.WriteLine("aradığınız bilgi listede mevcut");
+            else Console.WriteLine("aradığınız bilgi listede mevcut değil");
+            
+            Console.ReadLine();
+        }
+    }
+}
