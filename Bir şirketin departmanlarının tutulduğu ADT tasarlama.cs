1f2f6034d1ebdec39fa6ffec37d094a7c5ede3f5
rzy759 <39853524+rzy759@users.noreply.github.com>
2018-08-11 07:07:41 +0300

    Add files via upload

---
+++ Bir şirketin departmanlarının tutulduğu ADT tasarlama.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+    class departmanlar
+    {
+        public int depatmanİsmi { get; set; }
+        public int depatmanNo { get; set; }
+        public int depatmanBütcesi { get; set; }
+        public int depatmanYönetici { get; set; }
+
+        public static ArrayList kayıtEkle(departmanlar departman)
+        {
+            ArrayList kayıt = new ArrayList();
+            kayıt.Add(departman.depatmanİsmi);
+            kayıt.Add(departman.depatmanNo);
+            kayıt.Add(departman.depatmanBütcesi);
+            kayıt.Add(departman.depatmanYönetici);
+
+            return kayıt;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
